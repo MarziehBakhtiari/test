aa740c6c8c1360028ff5315d4ae2c39de3c82e3d
Marzieh <bakhtiari.electric.mapcm@gmail.com>
2023-07-22 10:46:59 +0330

    my first adding

---
+++ loope.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
+# get the start time
+st = time.time()
+
+for  i in range (1000000000):
+    # if (i%2548==0 and i%2546==0 and i> 97000000):
+    if (i>100000000 and i<100000002 ):
+        print (" i = " ,i)
+
+# get the end time
+et = time.time()
+
+# get the execution time
+elapsed_time = et - st
+print('Execution time:', elapsed_time, 'seconds')
